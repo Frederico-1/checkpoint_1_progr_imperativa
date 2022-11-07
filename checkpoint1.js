89d71fdb10108e937b4551c0c9ff24d17284f9e0
Frederico-1 <fredericoqsp.eventos@gmail.com>
2022-11-07 20:44:57 -0300

    checkpoint1.js
    Primeiro checkpoint de Programação Imperativa

---
+++ checkpoint1.js
@@ -0,0 +1,83 @@
+let comida = "brigadeiro";
+let tempo = 24;
+
+if (comida == "pipoca" && tempo == 10){
+    console.log("Prato pronto, bon appétit!");
+} 
+else if (comida == "pipoca" && tempo == 20){
+    console.log("Sua comida queimou!!!");
+}
+else if (comida == "pipoca" && tempo < 10){
+    console.log("Tempo insuficiente");
+}
+else if (comida == "pipoca" && tempo >= 30){
+    console.log("Kabummmmm!!!");
+}
+else if (comida == "macarrão" && tempo == 8){
+    console.log("Prato pronto, bon appétit!");
+} 
+else if (comida == "macarrão" && tempo == 16){
+    console.log("Sua comida queimou!!!");
+}
+else if (comida == "macarrão" && tempo < 8){
+    console.log("Tempo insuficiente");
+}
+else if (comida == "macarrão" && tempo >= 24){
+    console.log("Kabummmmm!!!");
+}
+
+else if (comida == "carne" && tempo == 15){
+    console.log("Prato pronto, bon appétit!");
+} 
+else if (comida == "carne" && tempo == 30){
+    console.log("Sua comida queimou!!!");
+}
+else if (comida == "carne" && tempo < 15){
+    console.log("Tempo insuficiente");
+}
+else if (comida == "carne" && tempo >= 45){
+    console.log("Kabummmmm!!!");
+}
+
+else if (comida == "feijão" && tempo == 12){
+    console.log("Prato pronto, bon appétit!");
+} 
+else if (comida == "feijão" && tempo == 24){
+    console.log("Sua comida queimou!!!");
+}
+else if (comida == "feijão" && tempo < 12){
+    console.log("Tempo insuficiente");
+}
+else if (comida == "feijão" && tempo >= 36){
+    console.log("Kabummmmm!!!");
+}
+
+else if (comida == "brigadeiro" && tempo == 8){
+    console.log("Prato pronto, bon appétit!");
+} 
+else if (comida == "brigadeiro" && tempo == 16){
+    console.log("Sua comida queimou!!!");
+}
+else if (comida == "brigadeiro" && tempo < 8){
+    console.log("Tempo insuficiente");
+}
+else if (comida == "brigadeiro" && tempo >= 24){
+    console.log("Kabummmmm!!!");
+}
+else{
+    console.log("Prato inexistente")
+}
+
+
+
+
+
+/*
+|| (comida == "macarrão" && tempo == 8) || (comida == "carne" && tempo == 15) || (comida == "feijão" && tempo == 12) || (comida == "brigadeiro" && tempo == 8)){
+        console.log("Prato pronto, bon appétit!");
+    }
+    if else { tempo
+        console.log("Deu ruim");
+    }
+
+*/
